7e1b05de5aa463358777dd8c09291db548c71457
Cassidy <cassidybridges@gmail.com>
2021-10-05 00:30:12 -0700

    pin deps

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1',
     description='HX711 Python Library for Raspberry Pi',
     py_modules=['hx711'],
-    install_requires=['Rpi.GPIO', 'numpy'],
+    install_requires=['cython==0.29.24','Rpi.GPIO==0.7.0', 'numpy==1.21.1'],
 )
 
